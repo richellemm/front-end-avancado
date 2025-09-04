ad27c3c0833453a4fe63be3549666d04332134d4
Richelle de Macêdo Monteiro <richellemonteiro@gmail.com>
2025-09-03 21:04:54 -0300

    aula02

---
+++ aula02/src/components/TrabalhandoComImagens.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Pokemon2 from '../assets/pokemon2.jpg'
+
+
+const TrabalhandoComImagens = () => {
+  return (
+    <div>
+        <img src ='pokemon.png' alt="" />
+        <img src={Pokemon2} alt="" />
+    </div>
+  )
+}
+
+export default TrabalhandoComImagens
