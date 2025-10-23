56b280ce934293c4b073d880a3988794764b9273
Richelle de Macêdo Monteiro <richellemonteiro@gmail.com>
2025-10-22 21:05:01 -0300

    aula 05 e 05_2

---
+++ aula05_2/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ListaDeAlunos from './components/ListaDeAlunos'
+
+const App = () => {
+  return (
+    <div>
+      <ListaDeAlunos></ListaDeAlunos>
+    </div>
+  )
+}
+
+export default App
