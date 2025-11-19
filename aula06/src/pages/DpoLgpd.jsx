31824a0b14a30cbcfc5017b291865b5b3f906873
Richelle de Macêdo Monteiro <richellemonteiro@gmail.com>
2025-11-18 21:38:14 -0300

    aula 12-11

--- aula06/src/pages/DpoLgpd.jsx
+++ aula06/src/pages/DpoLgpd.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const DpoLgpd = () => {
+
+    const [dpo, setDpo] = useState([])
+
+  useEffect(() => {
+    axios.get("http://localhost:3000/dpolgpd").then(response => setDpo(response.data)).catch(error => console.error("Erro ao carregar a lista dpo lgpd: ", error))
+  }, [])
+  
   return (
-    <div>Essa vai ser a página de LGPD</div>
+    <div>
+      <h1> DPO LGPD:</h1>
+      {
+        dpo.map((dpo) => (
+          <div key={dpo.id}>
+            {dpo.texto}
+          </div>
+        ))
+      }
+    </div>
   )
 }
 
